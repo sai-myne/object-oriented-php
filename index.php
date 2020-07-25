5779106fff4fda640cd493ee9d3442e8f15c35c0
sai-myne <ahlusir@gmail.com>
2020-07-25 10:01:34 +0800

    intro to classes

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php 
 
-echo 'hello, world';
+    class User {
+
+        // properties methods
+
+    }
+
+    $userOne = new User();
+    $userTwo = new User();
+
+    echo 'the class is ' .get_class($userTwo);
 
 ?>
 
@@ -8,7 +17,7 @@ echo 'hello, world';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP OOP Tutorial</title>
 </head>
 <body>
     
