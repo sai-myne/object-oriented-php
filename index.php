e75a0e57ddc00a2b1697cfedadfcd5e72d33b43e
sai-myne <ahlusir@gmail.com>
2020-07-25 10:13:59 +0800

    Properties & Methods

--- index.php
+++ index.php
@@ -2,14 +2,34 @@
 
     class User {
 
-        // properties methods
+        public $username = 'ryu';
+        public $email = 'ryu@thenetninja.co.uk';
+
+        public function addFriend(){
+            
+            return "$this->email added a new friend";
+        }
 
     }
 
     $userOne = new User();
     $userTwo = new User();
 
-    echo 'the class is ' .get_class($userTwo);
+    echo $userOne->username . '<br>';
+    echo $userOne->email . '<br>';
+    echo $userOne->addFriend() . '<br>';
+
+    $userTwo->username = 'yoshi';
+    $userTwo->email = 'yoshi@thenetninja.co.uk';
+
+    // print_r(get_class_vars('User'));
+    // print_r(get_class_methods('User'));
+
+    echo $userTwo->username . '<br>';
+    echo $userTwo->email . '<br>';
+    echo $userTwo->addFriend() . '<br>';
+
+
 
 ?>
 
